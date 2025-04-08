d864cd9d9e7a9e3258bc05780aa1b852703dbd8c
Jackeline Bomfim <jackeline.bomfim644@gmail.com>
2025-04-07 21:44:38 -0300

    Exercício um completo

--- contaBanco/src/ContaTerminal.java
+++ contaBanco/src/ContaTerminal.java
@@ -1,13 +1,19 @@
 
 
+
+
 public class ContaTerminal extends Conta {
     public static void main(String[] args) throws Exception {
 
-        Conta conta = new Conta();
+        System.out.println("Digite o número da Conta: ");
+        int conta = Integer.parseInt(System.console().readLine());    
+        System.out.println("Digite o número da Agência: ");
+        String agencia = System.console().readLine();     
+        System.out.println("Digite o nome do Nome do Cliente: ");
+        String nome = System.console().readLine(); 
+        System.out.println("Digite o saldo da Conta: ");
+        double saldo = Double.parseDouble(System.console().readLine());
 
-
-        
-        
-
-    }
+        System.out.println("Olá " + nome + ", obrigado por criar uma conta em nosso banco, sua agência é " + agencia + ", conta " + conta + " e seu saldo " + saldo + " já está disponível para saque.");
+    }   
 }
